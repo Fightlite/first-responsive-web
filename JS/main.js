21fd6d618ef94edf7e121d588495d8f9c0f8a6bf
Fightlite <Fightlite@github.com>
2021-09-05 22:27:59 +0700

    Update new SCSS files

--- JS/main.js
+++ JS/main.js
@@ -28,7 +28,7 @@ function scrollActive(){
 
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
+        const sectionTop = (current.getBoundingClientRect().top + window.pageYOffset) -50
         sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
@@ -41,7 +41,7 @@ function scrollActive(){
 }
 window.addEventListener('scroll', scrollActive)
 
-/*================== CHANGE BACKGROUND HEADER ======================*/
+/*================== CHANGE SCROLL HEADER ======================*/
 function scrollHeader(){
     const nav = document.getElementById('header')
     //When the scroll is greater than 200 viewport height, add the scroll header class//
@@ -64,13 +64,13 @@ const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'bx-sun'
 
-// Previously selected topic if user selected
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
-
 // We obtain the current theme that the interface has by validating the darktheme class
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark':'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon':'bx-sun'
+
+// Previously selected topic if user selected
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
 
 // We validate if the user previously chose a topic
 if(selectedTheme){
